c3b7ad8b1b0c0ff372dc7200880d962ec7daf8b4
ZhangYanPeng <cn.yanpengzhang@qq.com>
2017-11-23 18:17:22 +0800

    new project

--- TPIS/MainWindow.xaml.cs
+++ TPIS/MainWindow.xaml.cs
@@ -22,11 +22,39 @@ namespace TPIS
     public partial class MainWindow : Window
     {
         public ProjectControl projectList;
+        public ProjectItem currentPoject;
 
         public MainWindow()
         {
             InitializeComponent();
             projectList = new ProjectControl();
+            tab_project.SelectionChanged += new SelectionChangedEventHandler(Project_Change);
+        }
+
+        public void Project_Change(object sender, RoutedEventArgs e)
+        {
+            TabItem tis = (TabItem)tab_project.SelectedItem;
+            foreach(TabItem ti in tab_project.Items)
+            {
+                DockPanel dp = (DockPanel)ti.Header;
+                foreach (DependencyObject child in dp.Children)
+                {
+                    if (child is Button)
+                    {
+                        if (ti.Name == tis.Name)
+                        {
+                            //选中tab
+                            ((Button)child).Visibility = Visibility.Visible;
+                        }
+                        else {
+                            //非选中tab
+                            ((Button)child).Visibility = Visibility.Hidden;
+                        }
+                    }
+                }
+            }
+            //设置当前工程
+            currentPoject = projectList.projects[tab_project.SelectedIndex];
         }
 
         private void New_Project(object sender, RoutedEventArgs e)
@@ -36,5 +64,10 @@ namespace TPIS
             new_project.ShowDialog();
         }
         
+        public void Active_Project(int num)
+        {
+            //激活Tab
+            tab_project.SelectedIndex = num;
+        }
     }
 }
--- TPIS/NewProject.xaml.cs
+++ TPIS/NewProject.xaml.cs
@@ -20,6 +20,8 @@ namespace TPIS
     /// </summary>
     public partial class NewProject : Window
     {
+        MainWindow mainwin = (MainWindow)Application.Current.MainWindow;
+
         public NewProject()
         {
             InitializeComponent();
@@ -30,6 +32,7 @@ namespace TPIS
             this.Close();
         }
 
+        //新建程序
         private void Create(object sender, RoutedEventArgs e)
         {
             try
@@ -37,18 +40,52 @@ namespace TPIS
                 int.Parse(canvas_height.Text);
                 int.Parse(canvas_width.Text);
             }
-            catch(Exception exp)
+            catch (Exception exp)
             {
                 MessageBoxResult dr = MessageBox.Show("高度和宽度只能为整数", "提示", MessageBoxButton.OKCancel);
                 return;
             }
 
-            MainWindow mainwin = (MainWindow)Application.Current.MainWindow;
-            mainwin.projectList.projects.Add(new ProjectItem(int.Parse(canvas_height.Text), int.Parse(canvas_width.Text), project_name.Text));
-            Console.WriteLine(mainwin.projectList.projects.Count());
+            mainwin.projectList.projects.Add(new ProjectItem(int.Parse(canvas_height.Text), int.Parse(canvas_width.Text), project_name.Text + ".tpis"));
+            //插入tab
+            TabItem ti = AddTab(mainwin.projectList.projects.Last(), mainwin.projectList.projects.Count-1);
+            mainwin.tab_project.Items.Add(ti);
+
+            mainwin.Active_Project(mainwin.projectList.projects.Count-1);
             this.Close();
         }
 
+        private TabItem AddTab(ProjectItem project, int num)
+        {
+            TabItem ti = new TabItem();//< TabItem Header = "project.tpis" Name = "tabItem1" >
+            ti.Name = "tabItem" + num.ToString();
+
+            TextBlock tb = new TextBlock();
+            tb.Text = project.name;
+            Button bt = new Button();
+            Image img = new Image();
+            img.Source = new BitmapImage(new Uri("res/icon/tab_close.png", UriKind.Relative));
+            img.Width = 10;
+            img.Height = 10;
+            bt.Content = img;
+            bt.Visibility = Visibility.Hidden;
+
+            DockPanel dp = new DockPanel();
+            dp.Name = "project_header" + num.ToString();
+            DockPanel.SetDock(bt, Dock.Right);
+            dp.Children.Add(bt);
+            dp.Children.Add(tb);
+            dp.MinWidth = 30;
+            dp.Width = 90;
+            ti.Header = dp;
+
+            Canvas cs = new Canvas();//< Canvas Name = "MainCanvas" Background = "Gray" ></ Canvas >
+            cs.Name = "canvas" + num.ToString();
+            cs.Background = new SolidColorBrush(Colors.Gray);
+            ti.Content = cs;
+            
+            return ti;
+        }
 
     }
 }
