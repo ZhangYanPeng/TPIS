11ffa9e6dc375d75d1d02a8d45e75e6b681e23fd
Cowsboy <cwsbyab@126.cm>
2017-11-26 18:06:34 +0800

    工具箱

---
+++ TPIS/ControlTool.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TPIS
+{
+    /// <summary>
+    /// ControlTool.xaml 的交互逻辑
+    /// </summary>
+    public partial class ControlTool : Window
+    {
+        MainWindow mainwin = (MainWindow)Application.Current.MainWindow;
+
+        public ControlTool()
+        {
+            InitializeComponent();
+        }
+
+        private void rect_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            Rectangle rect = new Rectangle();
+            rect.Width = 50;
+            rect.Height = 50;
+            rect.Stroke = Brushes.Black;
+            mainwin.cs.Children.Add(rect);
+        }
+    }
+}
