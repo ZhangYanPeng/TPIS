72d9e26f5aceaa129abd375c7c4279b1f3e3b2fc
xjtu_lw <cowsboyab@126.com>
2018-01-13 20:10:48 +0800

    polyline

--- TPIS/TPISCanvas/ProjectDesignerCanvas.cs
+++ TPIS/TPISCanvas/ProjectDesignerCanvas.cs
@@ -15,6 +15,12 @@ namespace TPIS.TPISCanvas
 {
     public partial class ProjectDesignerCanvas : Canvas
     {
+        bool flag = false;
+        public CheckBox checkBox;//画线选择框
+        public List<Polyline> plines;//折线集合
+        private int count = -1;
+        public Point p1, p2, tmp;
+
         private SelectionService selectionService;
         internal SelectionService SelectionService
         {
@@ -26,6 +32,7 @@ namespace TPIS.TPISCanvas
                 return selectionService;
             }
         }
+
         public ProjectDesignerCanvas()
         {
             checkBox = new CheckBox();
@@ -36,64 +43,53 @@ namespace TPIS.TPISCanvas
 
         }
 
-        protected override void OnMouseMove(MouseEventArgs e)
-        {
-            base.OnMouseMove(e);
-            base.MouseMove += new MouseEventHandler(Canvas_MouseMove);
-        }
-
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
         {
             base.OnMouseLeftButtonDown(e);
-            base.MouseLeftButtonDown += new MouseButtonEventHandler(Canvas_MouseLeftButtonDown);
-        }
-
-        protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
-        {
-            base.OnMouseLeftButtonDown(e);
-            base.MouseRightButtonDown += new MouseButtonEventHandler(Canvas_MouseRightButtonDown);
-        }
-
-        /// <summary>
-        /// 画折线
-        /// </summary>
-        /// 
-
-        bool flag = false;
-        public CheckBox checkBox;//是否画线
-        public List<Polyline> plines;//画多条折线
-        private int count = -1;
-
-        private void Canvas_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (flag == false)
-                return;
-            plines[count].Points[plines[count].Points.Count - 1] = e.GetPosition(this);
-        }
-
-        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (checkBox.IsChecked==true)
+            //base.MouseLeftButtonDown += new MouseButtonEventHandler(Canvas_MouseLeftButtonDown);//会出现线性进入问题
+            if (checkBox.IsChecked == true)
             {
+                /*首击左键确定起点*/
                 if (flag == false)
                 {
                     flag = true;//开始画线
-                    count++;//只有首击左键时计数增1
+                    count++;//一条折线画完-计数增1
+
                     Polyline pline = new Polyline();
                     pline.Stroke = Brushes.Black;
                     pline.StrokeThickness = 2;
-                    this.Children.Add(pline);
+
                     plines.Add(pline);
+                    this.Children.Add(plines[count]);
+
+                    p1 = e.GetPosition(this);
+                    plines[count].Points.Add(p1);//起点
+                    plines[count].Points.Add(p1);//初始化折线（四个点）
+                    plines[count].Points.Add(p1);
+                    plines[count].Points.Add(p1);
                 }
-                plines[count].Points.Add(e.GetPosition(this));
-                if (plines[count].Points.Count == 1)
-                    plines[count].Points.Add(e.GetPosition(this));                
+                /*再击左键固定终点*/
+                else
+                {
+                    flag = false;//结束画线
+                }
             }
         }
 
-        private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        protected override void OnMouseMove(MouseEventArgs e)
         {
-            flag = false;//结束画线
+            base.OnMouseMove(e);
+            //base.MouseMove += new MouseEventHandler(Canvas_MouseMove);
+            if (flag == false)
+                return;
+            /*移动中确定拐点和终点*/
+            p2 = e.GetPosition(this);
+            tmp.X = (p1.X + p2.X) / 2;
+            tmp.Y = p1.Y;
+            plines[count].Points[1] = tmp;//拐点1
+            tmp.Y = p2.Y;
+            plines[count].Points[2] = tmp;//拐点2
+            plines[count].Points[3] = e.GetPosition(this);//终点
         }
     }
 }
