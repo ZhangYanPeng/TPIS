7ed75ce2682e6a747375fbd9fd13871e44e13963
ZhangYanPeng <cn.yanpengzhang@qq.com>
2018-01-15 11:25:26 +0800

    merge

--- TPIS/TPISCanvas/ProjectDesignerCanvas.cs
+++ TPIS/TPISCanvas/ProjectDesignerCanvas.cs
@@ -29,131 +29,6 @@ namespace TPIS.TPISCanvas
             }
         }
 
-        //protected override void OnMouseDown(MouseButtonEventArgs e)
-        //{
-        //    base.OnMouseDown(e);
-        //    if (e.Source == this)
-        //    {
-        //        // in case that this click is the start of a 
-        //        // drag operation we cache the start point
-        //        this.rubberbandSelectionStartPoint = new Point?(e.GetPosition(this));
-
-        //        // if you click directly on the canvas all 
-        //        // selected items are 'de-selected'
-        //        SelectionService.ClearSelection();
-        //        Focus();
-        //        e.Handled = true;
-        //    }
-        //}
-
-        //protected override void OnMouseMove(MouseEventArgs e)
-        //{
-        //    base.OnMouseMove(e);
-
-        //    // if mouse button is not pressed we have no drag operation, ...
-        //    if (e.LeftButton != MouseButtonState.Pressed)
-        //        this.rubberbandSelectionStartPoint = null;
-
-        //    // ... but if mouse button is pressed and start
-        //    // point value is set we do have one
-        //    if (this.rubberbandSelectionStartPoint.HasValue)
-        //    {
-        //        // create rubberband adorner
-        //        AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(this);
-        //        if (adornerLayer != null)
-        //        {
-        //            RubberbandAdorner adorner = new RubberbandAdorner(this, rubberbandSelectionStartPoint);
-        //            if (adorner != null)
-        //            {
-        //                adornerLayer.Add(adorner);
-        //            }
-        //        }
-        //    }
-        //    e.Handled = true;
-        //}
-
-        //protected override void OnDrop(DragEventArgs e)
-        //{
-        //    base.OnDrop(e);
-        //    DragObject dragObject = e.Data.GetData(typeof(DragObject)) as DragObject;
-        //    if (dragObject != null && !String.IsNullOrEmpty(dragObject.Xaml))
-        //    {
-        //        ProjectDesignerItem newItem = null;
-        //        Object content = XamlReader.Load(XmlReader.Create(new StringReader(dragObject.Xaml)));
-
-        //        if (content != null)
-        //        {
-        //            newItem = new ProjectDesignerItem();
-        //            newItem.Content = content;
-
-        //            Point position = e.GetPosition(this);
-
-        //            if (dragObject.DesiredSize.HasValue)
-        //            {
-        //                Size desiredSize = dragObject.DesiredSize.Value;
-        //                newItem.Width = desiredSize.Width;
-        //                newItem.Height = desiredSize.Height;
-
-        //                ProjectDesignerCanvas.SetLeft(newItem, Math.Max(0, position.X - newItem.Width / 2));
-        //                ProjectDesignerCanvas.SetTop(newItem, Math.Max(0, position.Y - newItem.Height / 2));
-        //            }
-        //            else
-        //            {
-        //                ProjectDesignerCanvas.SetLeft(newItem, Math.Max(0, position.X));
-        //                ProjectDesignerCanvas.SetTop(newItem, Math.Max(0, position.Y));
-        //            }
-
-        //            Canvas.SetZIndex(newItem, this.Children.Count);
-        //            this.Children.Add(newItem);
-        //            SetConnectorDecoratorTemplate(newItem);
-
-        //            //update selection
-        //            //this.SelectionService.SelectItem(newItem);
-        //            newItem.Focus();
-        //        }
-
-        //        e.Handled = true;
-        //    }
-        //}
-
-        //protected override Size MeasureOverride(Size constraint)
-        //{
-        //    Size size = new Size();
-
-        //    foreach (UIElement element in this.InternalChildren)
-        //    {
-        //        double left = Canvas.GetLeft(element);
-        //        double top = Canvas.GetTop(element);
-        //        left = double.IsNaN(left) ? 0 : left;
-        //        top = double.IsNaN(top) ? 0 : top;
-
-        //        //measure desired size for each child
-        //        element.Measure(constraint);
-
-        //        Size desiredSize = element.DesiredSize;
-        //        if (!double.IsNaN(desiredSize.Width) && !double.IsNaN(desiredSize.Height))
-        //        {
-        //            size.Width = Math.Max(size.Width, left + desiredSize.Width);
-        //            size.Height = Math.Max(size.Height, top + desiredSize.Height);
-        //        }
-        //    }
-        //    // add margin 
-        //    size.Width += 10;
-        //    size.Height += 10;
-        //    return size;
-        //}
-
-        //private void SetConnectorDecoratorTemplate(ProjectDesignerItem item)
-        //{
-        //    if (item.ApplyTemplate() && item.Content is UIElement)
-        //    {
-        //        ControlTemplate template = ProjectDesignerItem.GetConnectorDecoratorTemplate(item.Content as UIElement);
-        //        Control decorator = item.Template.FindName("PART_ConnectorDecorator", item) as Control;
-        //        if (decorator != null && template != null)
-        //            decorator.Template = template;
-        //    }
-        //}
-
 
         /// <summary>
         /// 初始化函数，添加画布事件
@@ -202,36 +77,86 @@ namespace TPIS.TPISCanvas
         public List<Polyline> plines;//画多条折线
         private int count = -1;
 
-        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        private SelectionService selectionService;
+        internal SelectionService SelectionService
         {
-            if (flag == false)
-                return;
-            plines[count].Points[plines[count].Points.Count - 1] = e.GetPosition(this);
-        }
-
-        private void Canvas_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        {
-            if (checkBox.IsChecked==true)
+            get
             {
-                if (flag == false)
-                {
-                    flag = true;//开始画线
-                    count++;//只有首击左键时计数增1
-                    Polyline pline = new Polyline();
-                    pline.Stroke = Brushes.Black;
-                    pline.StrokeThickness = 2;
-                    this.Children.Add(pline);
-                    plines.Add(pline);
-                }
-                plines[count].Points.Add(e.GetPosition(this));
-                if (plines[count].Points.Count == 1)
-                    plines[count].Points.Add(e.GetPosition(this));                
+                if (selectionService == null)
+                    selectionService = new SelectionService(this);
+
+                return selectionService;
             }
         }
 
-        private void Canvas_MouseRightButtonDown(object sender, MouseButtonEventArgs e)
+        public ProjectDesignerCanvas()
         {
-            flag = false;//结束画线
+            checkBox = new CheckBox();
+            checkBox.Content = "画线";
+            this.Children.Add(checkBox);
+
+            plines = new List<Polyline>();
+
         }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            base.OnMouseLeftButtonDown(e);
+            //base.MouseLeftButtonDown += new MouseButtonEventHandler(Canvas_MouseLeftButtonDown);//会出现线性进入问题
+            if (checkBox.IsChecked == true)
+            {
+                /*首击左键确定起点*/
+                if (flag == false)
+                {
+                    flag = true;//开始画线
+                    count++;//一条折线画完-计数增1
+
+                    Polyline pline = new Polyline();
+                    pline.Stroke = Brushes.Black;
+                    pline.StrokeThickness = 2;
+
+                    plines.Add(pline);
+                    this.Children.Add(plines[count]);
+
+                    p1 = e.GetPosition(this);
+                    plines[count].Points.Add(p1);//起点
+                    plines[count].Points.Add(p1);//初始化折线（四个点）
+                    plines[count].Points.Add(p1);
+                    plines[count].Points.Add(p1);
+                }
+
+                else
+                {
+                    flag = false;//结束画线
+                }
+                ///*再击左键固定终点*/
+                //if (flag == true && (ConsoleModifiers.Shift == 0))
+                //{
+
+                //}
+            }
+        }
+
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            //base.MouseMove += new MouseEventHandler(Canvas_MouseMove);
+            if (flag == false)
+                return;
+            /*移动中确定拐点和终点*/
+            p2 = e.GetPosition(this);
+            tmp.X = (p1.X + p2.X) / 2;
+            tmp.Y = p1.Y;
+            plines[count].Points[1] = tmp;//拐点1
+            tmp.Y = p2.Y;
+            plines[count].Points[2] = tmp;//拐点2
+            plines[count].Points[3] = e.GetPosition(this);//终点
+        }
+
+        //protected override void OnMouseRightButtonDown(MouseButtonEventArgs e)
+        //{
+        //    base.OnMouseRightButtonDown(e);
+        //    flag = false;//结束画线
+        //}
     }
 }
